65ee515789cb2284c062eeb24cf0e12d23d72c17
JClerve <joesonclerve9@gmail.com>
2025-03-22 22:01:22 +0530

    user repository is created

---
+++ src/main/java/com/example/SpringBoot_CRUD/Repository/userRepository.java
@@ -0,0 +1,11 @@
+package com.example.SpringBoot_CRUD.Repository;
+
+import com.example.SpringBoot_CRUD.Entity.userEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//Here in this entity, going to give the 2 arguments entity name, ID type that mention in entity
+public interface userRepository extends JpaRepository <userEntity,Long> {
+
+
+}
